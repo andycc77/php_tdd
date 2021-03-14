6000b23e135e2429748285b87988086aab18ec00
ChenAllen <andycc77e@gmail.com>
2021-03-14 16:02:27 +0800

    test view single question

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -4,9 +4,11 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Question;
 
 class ViewQuestionsTest extends TestCase
 {
+    use RefreshDatabase;
 
     /** @test  */
     public function user_can_view_questions()
@@ -19,5 +21,20 @@ class ViewQuestionsTest extends TestCase
         $test = $this->get('/questions');
         //3. 正常返回 200
         $test->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        //1.創建一個問題
+        $question = factory(Question::class)->create();
+
+        //2.訪問鏈結
+        $test = $this->get('/questions/' . $question->id);
+
+        //3. 那麼應該看到問題的內容
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
