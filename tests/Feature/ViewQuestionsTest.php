d2aee1c7f6830f2adf2f91d6b59f94ed14e541e7
vagrant <vagrant@homestead>
2021-03-14 04:35:37 +0000

    test view questions

---
+++ tests/Feature/ViewQuestionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewQuestionsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        //0. 拋出異常
+        $this->withoutExceptionHandling();
+
+        //1.假設 /questions 路由存在
+        //2. 訪問鏈結 /questions
+        $test = $this->get('/questions');
+        //3. 正常返回 200
+        $test->assertStatus(200);
+    }
+}
