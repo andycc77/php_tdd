62582abf4e4370679adf724e064224d7bb6142b0
ChenAllen <andycc77e@gmail.com>
2021-03-21 03:07:07 +0800

    refactory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -4,14 +4,25 @@
 
 use App\Question;
 use App\User;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        'user_id' => function () {
-            return factory(User::class)->create()->id;
-        },
+        'user_id' => factory(User::class),
         'title' => $faker->sentence,
         'content' => $faker->text
+    ];
+});
+
+$factory->state(Question::class, 'published', function ($faker) {
+    return [
+        'published_at' => Carbon::parse('-1 week'),
+    ];
+});
+
+$factory->state(Question::class,'unpublished',function ($faker) {
+    return [
+        'published_at' => null
     ];
 });
